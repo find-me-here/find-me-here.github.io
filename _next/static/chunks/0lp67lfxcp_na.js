d2c93c97798ed5b83bcfbe6c588e9577e52b67b2
suman98 <41943773+suman98@users.noreply.github.com>
2026-09-23 12:40:29 +0545

    Updates

---
+++ _next/static/chunks/0lp67lfxcp_na.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,34998,s=>{s.v(t=>Promise.all(["static/chunks/3r2bv0k88a_kj.js","static/chunks/0m7agmlojaz70.js","static/chunks/0nhouxbdskn93.js","static/chunks/0khtq480dd61h.css"].map(t=>s.l(t))).then(()=>t(23610)))},46681,function(s){s.n(s.i(89231))}]);
